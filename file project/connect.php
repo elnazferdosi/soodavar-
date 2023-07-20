73decfe1db8bb323048d3f33c1f8216031532e1c
elnazferdosi <aseman.7996@gmail.com>
2023-07-20 20:03:00 +0330

    update connect and database

--- file project/connect.php
+++ file project/connect.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "test";
+    $dbname = "soodavar";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
